51a3eddf5d0f76557b87d3e8349ca4316d0f3b5b
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2024-04-23 14:17:49 -0300

    Add files via upload

---
+++ cursos.js
@@ -0,0 +1,123 @@
+    export const products = [
+        {
+            "id": "1",
+            "productName": "Educação Infantil ",
+            "productPrice": "R$ 800.00 - período integral",
+            "productImg": ""
+        },
+        {
+            "id": "2",
+            "productName": "Ensino Fundamental I",
+            "productPrice": "R$ 900.00",
+            "productImg": ""
+        },
+        {
+            "id": "3",
+            "productName": "Ensino Fundamental II",
+            "productPrice": "R$ 950.00",
+            "productImg": ""
+        },
+        {
+            "id": "4",
+            "productName": "Ensino Médio",
+            "productPrice": "R$ 1100.00",
+            "productImg": ""
+        },
+        {
+            "id": "5",
+            "productName": "Curso de Reforco para Todas as Idades",
+            "productPrice": "110.00",
+            "productImg": "redes_computadores.png"
+        },
+        {
+            "id": "6",
+            "productName": "Introdução à Programação",
+            "productPrice": "50.00",
+            "productImg": ""
+        },
+        {
+            "id": "7",
+            "productName": "Web Development Bootcamp",
+            "productPrice": "150.00",
+            "productImg": ""
+        },
+        {
+            "id":"8",
+            "productName":"Curso Online de Fotografia",
+            "productPrice":"35.00",
+            "productImg":""
+        },
+        
+        {
+            "id": "9",
+            "productName": "Curso de Marketing Digital",
+            "productPrice": "R$ 300.00",
+            "productImg": ""
+        },
+        {
+            "id": "10",
+            "productName": "Curso de Administração de Empresas",
+            "productPrice": "R$ 450.00",
+            "productImg": ""
+        },
+       {
+            "id": "1",
+            "productName": "Curso de Design Gráfico",
+            "productPrice": "R$ 380.00",
+            "productImg": ""
+        },
+        {
+            "id": "12",
+            "productName": "Curso de Culinária",
+            "productPrice": "R$ 300.00",
+            "productImg": ""
+        },
+        {
+            "id": "13",
+            "productName": "Curso de Robótica",
+            "productPrice": "R$ 450.00",
+            "productImg": ""
+        },
+        {
+            "id": "14",
+            "productName": "Curso de Enfermagem",
+            "productPrice": "180.00",
+            "productImg": "enfermagem.png"
+        },
+        {
+            "id": "15",
+            "productName": "Curso Preparatório para Vestibular",
+            "productPrice": "120.00",
+            "productImg": "curso_preparatorio.png"
+        },
+        {
+            "id": "16",
+            "productName": "Curso de Desenvolvimento de Jogos",
+            "productPrice": "150.00",
+            "productImg": "desenvolvimento_jogos.png"
+        },
+        {
+            "id": "17",
+            "productName": "Curso de Programação em Python",
+            "productPrice": "110.00",
+            "productImg": "python_programacao.png"
+        },
+        {
+            "id": "18",
+            "productName": "Curso de Escrita Criativa",
+            "productPrice": "80.00",
+            "productImg": "escrita_criativa.png"
+        },
+        {
+            "id": "19",
+            "productName": "Curso de Gestão de Projetos",
+            "productPrice": "140.00",
+            "productImg": "gestao_projetos.png"
+        },
+        {
+            "id": "20",
+            "productName": "Curso de Redes de Computadores",
+            "productPrice": "110.00",
+            "productImg": "redes_computadores.png"
+        },
+    ]
