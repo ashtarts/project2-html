0f9de75cddf5661f0975dc0676b3e2eb01c314df
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2024-04-23 08:40:13 -0300

    Update items.js

--- items.js
+++ items.js
@@ -3,21 +3,21 @@ export const products = [
         "id":"1",
         "productName":"O Pequeno Príncipe",
         "productPrice":"25.00",
-        "productImg":"principe.png"
+        "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/principe.png?raw=true"
    },
 
     {
         "id":"2",
         "productName":"Dom Casmurro",
         "productPrice":"18.50",
-        "productImg":"dom.png"
+        "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/dom.png?raw=true"
    },
  
    {
     "id":"3",
     "productName":"Memórias Póstumas de Brás Cubas",
     "productPrice":"15.99",
-    "productImg":"bras.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/bras.png?raw=true"
     
    },
 
@@ -25,118 +25,118 @@ export const products = [
     "id":"4",
     "productName":"A Metamorfose",
     "productPrice":"12.00",
-    "productImg":"meta.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/meta.png?raw=true"
    },
 
    {
     "id":"5",
     "productName":"1984",
     "productPrice":"20.50",
-    "productImg":"1984.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/1984.png?raw=true"
    },
 
    {
     "id":"6",
     "productName":"Crime e Castigo",
     "productPrice":"30.00",
-    "productImg":"crime.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/crime.png?raw=true"
    },
 
    {
     "id":"7",
     "productName":"O Cortiço",
     "productPrice":"14.99",
-    "productImg":"cortico.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/cortico.png?raw=true"
    },
 
    {
     "id":"8",
     "productName":"O Senhor dos Anéis: A Sociedade do Anel",
     "productPrice":"35.00",
-    "productImg":"aneis.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/aneis.png?raw=true"
    },
 
    {
     "id":"9",
     "productName":"Harry Potter e a Pedra Filosofal",
     "productPrice":"24.99",
-    "productImg":"pedra.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/pedra.png?raw=true"
    },
 
    {
     "id":"10",
     "productName":"O Hobbit",
     "productPrice":"22.00",
-    "productImg":"hobbit.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/hobbit.png?raw=true"
    },
 
    {
     "id":"11",
     "productName":"Percy Jackson e o Ladrão de Raios",
     "productPrice":"16.50",
-    "productImg":"percy.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/percy.png?raw=true"
     },
 
     {
         "id":"12",
         "productName":"A Culpa é das Estrelas",
         "productPrice":"19.99",
-        "productImg":"culpa.png"
+        "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/culpa.png?raw=true"
     },
 
     {
     "id":"13",
     "productName":"A Cabana",
     "productPrice":"21.50",
-    "productImg":"cabana.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/cabana.png?raw=true"
     },
 
     {
     "id":"14",
     "productName":"O Alquimista",
     "productPrice":"17.00",
-    "productImg":"alquimista.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/alquimista.png?raw=true"
     },
 
     {
     "id":"15",
     "productName":"A Menina que Roubava Livros",
     "productPrice":"23.50",
-    "productImg":"roubava.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/roubava.png?raw=true"
     },
     {
     "id":"16",
     "productName":"O Diário de Anne Frank",
     "productPrice":"28.00",
-    "productImg":"anne.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/anne.png?raw=true"
     },
 
     {
     "id":"17",
     "productName":"O Primo Basílio",
     "productPrice":"15.00",
-    "productImg":"primo.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/primo.png?raw=true"
     },
 
     {
     "id":"18",
     "productName":"As Crônicas de Nárnia",
     "productPrice":"19.00",
-    "productImg":"narnia.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/narnia.png?raw=true"
     },
 
     {
     "id":"19",
     "productName":"Moby Dick",
     "productPrice":"26.50",
-    "productImg":"moby.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/moby.png?raw=true"
     },
 
     {
     "id":"20",
     "productName":"Alice no País das Maravilhas",
     "productPrice":"18.00",
-    "productImg":"alice.png"
+    "productImg":"https://github.com/ashtarts/project2-html/blob/main/img/alice.png?raw=true"
     }    
 
 ]
