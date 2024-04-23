21594ea1a9f09238f83eb6e9f8f483b5f3a153d4
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2024-04-23 08:13:26 -0300

    Add files via upload

---
+++ items.js
@@ -0,0 +1,142 @@
+export const products = [
+    {
+        "id":"1",
+        "productName":"O Pequeno Príncipe",
+        "productPrice":"25.00",
+        "productImg":"principe.png"
+   },
+
+    {
+        "id":"2",
+        "productName":"Dom Casmurro",
+        "productPrice":"18.50",
+        "productImg":"dom.png"
+   },
+ 
+   {
+    "id":"3",
+    "productName":"Memórias Póstumas de Brás Cubas",
+    "productPrice":"15.99",
+    "productImg":"bras.png"
+    
+   },
+
+   {
+    "id":"4",
+    "productName":"A Metamorfose",
+    "productPrice":"12.00",
+    "productImg":"meta.png"
+   },
+
+   {
+    "id":"5",
+    "productName":"1984",
+    "productPrice":"20.50",
+    "productImg":"1984.png"
+   },
+
+   {
+    "id":"6",
+    "productName":"Crime e Castigo",
+    "productPrice":"30.00",
+    "productImg":"crime.png"
+   },
+
+   {
+    "id":"7",
+    "productName":"O Cortiço",
+    "productPrice":"14.99",
+    "productImg":"cortico.png"
+   },
+
+   {
+    "id":"8",
+    "productName":"O Senhor dos Anéis: A Sociedade do Anel",
+    "productPrice":"35.00",
+    "productImg":"aneis.png"
+   },
+
+   {
+    "id":"9",
+    "productName":"Harry Potter e a Pedra Filosofal",
+    "productPrice":"24.99",
+    "productImg":"pedra.png"
+   },
+
+   {
+    "id":"10",
+    "productName":"O Hobbit",
+    "productPrice":"22.00",
+    "productImg":"hobbit.png"
+   },
+
+   {
+    "id":"11",
+    "productName":"Percy Jackson e o Ladrão de Raios",
+    "productPrice":"16.50",
+    "productImg":"percy.png"
+    },
+
+    {
+        "id":"12",
+        "productName":"A Culpa é das Estrelas",
+        "productPrice":"19.99",
+        "productImg":"culpa.png"
+    },
+
+    {
+    "id":"13",
+    "productName":"A Cabana",
+    "productPrice":"21.50",
+    "productImg":"cabana.png"
+    },
+
+    {
+    "id":"14",
+    "productName":"O Alquimista",
+    "productPrice":"17.00",
+    "productImg":"alquimista.png"
+    },
+
+    {
+    "id":"15",
+    "productName":"A Menina que Roubava Livros",
+    "productPrice":"23.50",
+    "productImg":"roubava.png"
+    },
+    {
+    "id":"16",
+    "productName":"O Diário de Anne Frank",
+    "productPrice":"28.00",
+    "productImg":"anne.png"
+    },
+
+    {
+    "id":"17",
+    "productName":"O Primo Basílio",
+    "productPrice":"15.00",
+    "productImg":"primo.png"
+    },
+
+    {
+    "id":"18",
+    "productName":"As Crônicas de Nárnia",
+    "productPrice":"19.00",
+    "productImg":"narnia.png"
+    },
+
+    {
+    "id":"19",
+    "productName":"Moby Dick",
+    "productPrice":"26.50",
+    "productImg":"moby.png"
+    },
+
+    {
+    "id":"20",
+    "productName":"Alice no País das Maravilhas",
+    "productPrice":"18.00",
+    "productImg":"alice.png"
+    }    
+
+]
