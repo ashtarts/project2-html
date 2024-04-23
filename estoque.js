4e8f42b3c2d5021b870647f38ec37f2e5a905e43
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2024-04-23 08:17:38 -0300

    Update estoque.js

--- estoque.js
+++ estoque.js
@@ -3,21 +3,21 @@ export const products = [
         "id":"1",
         "productName":"Uniforme Escolar - Camiseta",
         "productPrice":"20.00",
-        "productImg":"https://example.com/uniform-shirt.jpg"
+        "productImg":"uniforme.png"
    },
 
     {
         "id":"2",
         "productName":"Conjunto de Lápis de Cor",
         "productPrice":"10.50",
-        "productImg":"https://example.com/color-pencil-set.jpg"
+        "productImg":"cor.png"
    },
  
    {
     "id":"3",
     "productName":"Mochila Escolar",
     "productPrice":"25.99",
-    "productImg":"https://example.com/school-backpack.jpg"
+    "productImg":"mochila.png"
     
    },
 
@@ -25,118 +25,118 @@ export const products = [
     "id":"4",
     "productName":"Estojo Escolar",
     "productPrice":"5.00",
-    "productImg":"https://example.com/school-pencil-case.jpg"
+    "productImg":"estojo.png"
    },
 
    {
     "id":"5",
     "productName":"Uniforme Escolar - Calça",
     "productPrice":"25.00",
-    "productImg":"https://example.com/uniform-pants.jpg"
+    "productImg":"calça.png"
    },
 
    {
     "id":"6",
     "productName":"Caderno Universitário",
     "productPrice":"7.99",
-    "productImg":"https://example.com/notebook.jpg"
+    "productImg":"caderno.png"
    },
 
    {
     "id":"7",
     "productName":"Calculadora Científica",
     "productPrice":"15.99",
-    "productImg":"https://example.com/scientific-calculator.jpg"
+    "productImg":"calculadora.png"
    },
 
    {
     "id":"8",
     "productName":"Agenda Escolar",
     "productPrice":"8.00",
-    "productImg":"https://example.com/school-agenda.jpg"
+    "productImg":"agenda.png"
    },
 
    {
     "id":"9",
     "productName":"Tesoura Escolar",
     "productPrice":"3.50",
-    "productImg":"https://example.com/school-scissors.jpg"
+    "productImg":"tesoura.png"
    },
 
    {
     "id":"10",
     "productName":"Uniforme Escolar - Blusa de Frio",
     "productPrice":"30.00",
-    "productImg":"https://example.com/uniform-sweater.jpg"
+    "productImg":"frio.png"
    },
 
    {
     "id":"11",
     "productName":"Régua",
     "productPrice":"2.50",
-    "productImg":"https://example.com/ruler.jpg"
+    "productImg":"regua.png"
     },
 
     {
         "id":"12",
         "productName":"Lancheira Escolar",
         "productPrice":"12.99",
-        "productImg":"https://example.com/school-lunchbox.jpg"
+        "productImg":"lancheira.png"
     },
 
     {
     "id":"13",
     "productName":"Borracha",
     "productPrice":"1.00",
-    "productImg":"https://example.com/eraser.jpg"
+    "productImg":"borracha.png"
     },
 
     {
     "id":"14",
-    "productName":"Livro Didático de Matemática",
+    "productName":"Corretivo",
     "productPrice":"18.00",
-    "productImg":"https://example.com/math-textbook.jpg"
+    "productImg":"corretivo.png"
     },
 
     {
     "id":"15",
     "productName":"Canetas Esferográficas - Pacote com 10",
     "productPrice":"5.50",
-    "productImg":"https://example.com/ballpoint-pens.jpg"
+    "productImg":"caneta.png"
     },
     {
     "id":"16",
-    "productName":"Tesoura sem Ponta",
+    "productName":"Lapis de Escrita",
     "productPrice":"4.00",
-    "productImg":"https://example.com/safety-scissors.jpg"
+    "productImg":"lapis.png"
     },
 
     {
     "id":"17",
-    "productName":"Apontador de Lápis",
+    "productName":"Lapiseira",
     "productPrice":"1.50",
-    "productImg":"https://example.com/pencil-sharpener.jpg"
+    "productImg":"lapiseira.png"
     },
 
     {
     "id":"18",
     "productName":"Compasso",
     "productPrice":"3.00",
-    "productImg":"https://example.com/compass.jpg"
+    "productImg":"compasso.png"
     },
 
     {
     "id":"19",
-    "productName":"Tesoura de Ponto e Vírgula",
+    "productName":"Garrafa de Água",
     "productPrice":"4.50",
-    "productImg":"https://example.com/semicolon-scissors.jpg"
+    "productImg":"garrafa.png"
     },
 
     {
     "id":"20",
-    "productName":"Bolsa para Transporte de Material Escolar",
+    "productName":"Cola p/ Papel",
     "productPrice":"10.00",
-    "productImg":"https://example.com/school-material-bag.jpg"
+    "productImg":"cola.png"
     }    
 
 ]
