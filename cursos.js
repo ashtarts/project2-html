d3133762967f58d2a1065d1a2fb67a2f67884c94
Breno Oliveira <99503617+Brelin27@users.noreply.github.com>
2024-04-23 21:18:56 -0300

    Update cursos.js

--- cursos.js
+++ cursos.js
@@ -3,121 +3,121 @@
             "id": "1",
             "productName": "Educação Infantil ",
             "productPrice": "R$ 800.00 - período integral",
-            "productImg": ""
+            "productImg": "EduInf.png"
         },
         {
             "id": "2",
             "productName": "Ensino Fundamental I",
             "productPrice": "R$ 900.00",
-            "productImg": ""
+            "productImg": "EnsFun1.png"
         },
         {
             "id": "3",
             "productName": "Ensino Fundamental II",
             "productPrice": "R$ 950.00",
-            "productImg": ""
+            "productImg": "EnsFun2.png"
         },
         {
             "id": "4",
             "productName": "Ensino Médio",
             "productPrice": "R$ 1100.00",
-            "productImg": ""
+            "productImg": "EnsMed.png"
         },
         {
             "id": "5",
             "productName": "Curso de Reforco para Todas as Idades",
             "productPrice": "110.00",
-            "productImg": "redes_computadores.png"
+            "productImg": "Reforço.png"
         },
         {
             "id": "6",
             "productName": "Introdução à Programação",
             "productPrice": "50.00",
-            "productImg": ""
+            "productImg": "IntroProg.png"
         },
         {
             "id": "7",
             "productName": "Web Development Bootcamp",
             "productPrice": "150.00",
-            "productImg": ""
+            "productImg": "WebBoot.png"
         },
         {
             "id":"8",
             "productName":"Curso Online de Fotografia",
             "productPrice":"35.00",
-            "productImg":""
+            "productImg":"Cfoto.png"
         },
         
         {
             "id": "9",
             "productName": "Curso de Marketing Digital",
             "productPrice": "R$ 300.00",
-            "productImg": ""
+            "productImg": "CMarkDig.png"
         },
         {
             "id": "10",
             "productName": "Curso de Administração de Empresas",
             "productPrice": "R$ 450.00",
-            "productImg": ""
+            "productImg": "Cadm.png"
         },
        {
             "id": "1",
             "productName": "Curso de Design Gráfico",
             "productPrice": "R$ 380.00",
-            "productImg": ""
+            "productImg": "CDesgraf.png"
         },
         {
             "id": "12",
             "productName": "Curso de Culinária",
             "productPrice": "R$ 300.00",
-            "productImg": ""
+            "productImg": "Cculi.pg"
         },
         {
             "id": "13",
             "productName": "Curso de Robótica",
             "productPrice": "R$ 450.00",
-            "productImg": ""
+            "productImg": "Crob.png"
         },
         {
             "id": "14",
             "productName": "Curso de Enfermagem",
             "productPrice": "180.00",
-            "productImg": ""
+            "productImg": "Cenf.png"
         },
         {
             "id": "15",
             "productName": "Curso Preparatório para Vestibular",
             "productPrice": "120.00",
-            "productImg": ""
+            "productImg": "Cprep.png"
         },
         {
             "id": "16",
             "productName": "Curso de Desenvolvimento de Jogos",
             "productPrice": "150.00",
-            "productImg": ""
+            "productImg": "Cdj.png"
         },
         {
             "id": "17",
             "productName": "Curso de Programação em Python",
             "productPrice": "110.00",
-            "productImg": ""
+            "productImg": "Cpyt.png"
         },
         {
             "id": "18",
             "productName": "Curso de Escrita Criativa",
             "productPrice": "80.00",
-            "productImg": ""
+            "productImg": "CescriCria.png"
         },
         {
             "id": "19",
             "productName": "Curso de Gestão de Projetos",
             "productPrice": "140.00",
-            "productImg": ""
+            "productImg": "CgesProj.png"
         },
         {
             "id": "20",
             "productName": "Curso de Redes de Computadores",
             "productPrice": "110.00",
-            "productImg": ""
+            "productImg": "Credes.png"
         },
     ]
